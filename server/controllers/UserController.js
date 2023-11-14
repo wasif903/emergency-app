92f36d7a6995da28f13a14720c2ba43258fcd368
wasif903 <wasifmehmood903@gmail.com>
2023-11-14 18:44:34 +0500

    pushing changes

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -32,5 +32,21 @@ const HandleRegisterUser = async (req, res) => {
     }
 }
 
+const HandleLoginUser = async (req, res) => {
+    try {
+        const isUserExists = await UserSchema.findOne({ email: req.body.email });
+        if (!isUserExists) {
+            return res.status(404).json({ message: 'User Not Found' })
+        }
+        if (isUserExists.email === req.body.email && isUserExists.password === req.body.password) {
+            res.status(200).json({ message: "Logged In Successfully", user: isUserExists })
+        } else {
+            res.status(400).json({ message: "Invalid Username And Password" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
 
-export { HandleGetAllUsers, HandleRegisterUser };
+export { HandleGetAllUsers, HandleRegisterUser, HandleLoginUser };
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { HandleGetAllUsers, HandleRegisterUser } from "../controllers/UserController.js";
+import { HandleGetAllUsers, HandleLoginUser, HandleRegisterUser } from "../controllers/UserController.js";
 
 const router = express.Router();
 
 
-router.get("/get-users", HandleGetAllUsers)
+router.get("/get-users", HandleGetAllUsers);
 
+router.post("/user-signup", HandleRegisterUser);
 
-router.post("/user-signup", HandleRegisterUser)
+router.post("/login", HandleLoginUser);
 
 export default router;
